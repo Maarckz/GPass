cd4cd456b62cdaf26eb41dcd41862c4dae9f25b1
Maarckz <48658125+Maarckz@users.noreply.github.com>
2025-02-01 19:04:07 -0300

    Delete utils directory

--- utils/security_utils.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-def hash_password(password):
-    """Gera um hash seguro para a senha."""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
-def verify_password(hashed_password, input_password):
-    """Verifica se a senha fornecida corresponde ao hash."""
-    return bcrypt.checkpw(input_password.encode('utf-8'), hashed_password.encode('utf-8'))
